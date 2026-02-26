c433e1e4b9ceb18eb757edf08924b28f03298595
Molly (✿◠‿◠) <mollyannegustaf@gmail.com>
2026-02-26 13:35:51 -0700

    updates adding to main()

--- README.md
+++ README.md
@@ -7,10 +7,15 @@ check if you already have Python run the command 'python --version' in terminal
 
 enter cd to find out what folder / directory you're currently in
 navigate to the correct one, example: 
-if downloaded program to Downloadds on Windows enter command
+if downloaded program to Downloads on Windows enter command
+'cd Downloads'
 
-cd Downloads 
-
-
-*later update to include instructions if you use tabulate table for main()
+----------------------------------------------
  
+ You will also need to have requirements.txt installed using 'pip install -r requirements.txt' command in terminal
+ this txt file contains packages/libraries that need to be installed for this program - ie. tabulate
+
+ * you may need to make sure that pip is newest version - run '-m pip install --upgrade pip' in terminal if required
+
+-----------------------------------------------
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 
 class Budget: 
     def __init__(self):
@@ -16,74 +17,66 @@ class Budget:
         total_budget = float(income) - float(expenses)
         return total_budget 
     
+def display_menu(): #displays the tabulate menu when called
 
+    data = [   #data columns separated by comma
+    ["Update Income", 1],
+    ["Add Expense", 2],
+    ["Edit Expense", 3],
+    ["Display Balance", 4]
+        ]
+
+    headers = [  #headers separated by comma
+        "Menu", "Select Option" 
+        ]
+
+    print(tabulate(data, headers=headers, tablefmt="grid"))
+    
 
 def main():
     budget = Budget() #create an object from the budget class
+
+    print(" ")
+    print("Welcome to the terminal budget calculator!")
+
+    display_menu()
     
-    try: 
-        budget.income = float(input("What is your income?")) 
+    try:
+        user_selection = input("Select from the menu to proceed: ") #outside try except block is if user enters non numeric input
+        
+        if user_selection == "1": #UPDATE INCOME these are the sections that are based on user selections 
+            try: 
+                budget.income = float(input("What is your income?")) 
+                display_menu()
+                user_selection = input("Income updated. Select from the menu to proceed: ")
+            except ValueError:
+                print("Invalid input, just enter numeric value.")
+                budget.income = float(input("What is your income?")) 
+                display_menu()
+                user_selection = input("Income updated. Select from the menu to proceed: ")
+
+        if user_selection == "2": #ADD EXPENSE 
+            while True:
+                name = input("What is the expense name? (type x to exit no more expenses)")
+                if name == "x": 
+                    break
+                amount = float(input("What is the expense amount?"))
+                total_expenses = budget.total_expense(name, amount)
+                budget.balance(budget.income, total_expenses)
+            display_menu()
+            user_selection = input("Expense updated. Select from the menu to proceed: ")
+
+        if user_selection == "4": #DISPLAY BALANCE
+            print("Balance: ", budget.balance(budget.income, total_expenses))
+
     except ValueError:
-        print("Invalid input, just enter numeric value.")
-        budget.income = float(input("What is your income?")) 
+        print("Please enter a valid selection from the menu")
 
-    while True:
-        name = input("What is the expense name? (type x to exit no more expenses)")
-        if name == "x": 
-                break
-        amount = float(input("What is the expense amount?"))
-        total_expenses = budget.total_expense(name, amount) 
+    
 
-    print("Balance: ", budget.balance(budget.income, total_expenses))
 
 if __name__ == "__main__":
     main()
-
-#GET user input of menu option
-#DISPLAY confirmation of selection to UI - add error handling for invalid input?
-#IF user selects 1 THEN call expense()
-#IF user selects 2 THEN call expense()
-#IF user selects 3 THEN call income()
-#IF user selects 4 THEN call method()
-
-#IF expense() method is called AND selection 1, super().add() within expense subclass
-#LOAD new .csv file
-#GET user input for name and amount
-#SAVE .csv file
-#DISPLAY budget UI
-
-#IF expense() method is called AND selection 2, super().edit() within expense subclass
-#GET user input of which expense to edit
-#LOAD previous .csv file
-#GET user input for expense and amount
-#SAVE .csv file
-#DISPLAY budget UI
-
-#IF income() method is called AND selection 3, super().edit() within income subclass
-#LOAD previous .csv file
-#GET user input for name and amount
-#SAVE .csv file
-#DISPLAY budget UI
-
-# _____
-#IF user selects 'changes for income'
-#GET user selection from menu for income
-#CALL function for that menu input selection
-#GET user input for method called
-#UPDATE ledger
-#SAVE .csv file
-#DISPLAY budget UI
-
-#IF user selects 'changes for income'
-#AND selects new/edit income
-#GET user input for state of residence
-#CALCULATE net income after taxes using value of variable
-#UPDATE ledger
-#SAVE .csv file
-#DISPLAY budget UI
-
-
- 
 
 #testing better comments extension
 # TODO: oh cool
